aaeda12e54f792af8554e0dc67613ff987dff7c9
Hugo Speak <hugo.speak@hotmail.com>
2021-09-12 19:54:51 +0100

    RCV setup

---
+++ app/controllers/urls_controller.rb
@@ -0,0 +1,12 @@
+class UrlsController < ApplicationController
+
+  def index
+  end
+
+  def create
+  end
+
+  def external
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # index
+  root to: "urls#index"
+
+  # create new short url
+  post "/shorten", to: "urls#create"
+
+  # redirect to external address
+  get "/:id", to: "urls#external"
+
+  # API response
+  
+
 end
