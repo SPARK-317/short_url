0a15c5b3a0f5ce58e2ab8bf070d200adfa6e7d31
Hugo Speak <hugo.speak@hotmail.com>
2021-09-12 21:55:02 +0100

    styling

--- app/javascript/components/ajax_results.js
+++ app/javascript/components/ajax_results.js
@@ -3,7 +3,15 @@ const ajaxResults = () => {
   document.querySelector("#new_long").addEventListener("ajax:complete", (event) => {
     // console.log(event.detail[0].response);
     const response = event.detail[0].response;
-    document.querySelector("#result").innerText = response;
+    const result = document.querySelector("#result")
+    result.innerText = response;
+    result.className = "";
+    if (response == "INVALID URL") {
+      result.classList.add("alert-danger");
+    }
+    else {
+      result.classList.add("alert-success");
+    }
   });
 };
 
